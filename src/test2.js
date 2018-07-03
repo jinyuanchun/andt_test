670b880e0407e2743761adf0dc4219cb61b4ffb1
jinyuanchun <40689145+jinyuanchun@users.noreply.github.com>
2018-07-03 21:25:44 +0800

    Update test2.js

--- src/test2.js
+++ src/test2.js
@@ -1,16 +1,56 @@
 import React,{ Component } from 'react';	
 import PropTypes from 'prop-types';
-import {Button} from 'antd';
+
 import axios from 'axios';
 import get from './axios/get';
 
+import {Button,Table} from 'antd';
+import 'antd/dist/antd.css';
+
 var info;
+let self;
+
+const columns = [{
+	title:'子项目代码',
+	dataIndex:'subproject_id',
+	key:'subproject_id',
+},{
+	title:'子项目名称',
+	dataIndex:'subproject_name',
+	key:'subproject_name',
+},{
+	title:'合同代码',
+	dataIndex:'project_id',
+	key:'project_id',
+},{
+	title:'项目类型',
+	dataIndex:'ais_type',
+	key:'ais_type',
+},{
+	title:'分析类型',
+	dataIndex:'ais_analysis',
+	key:'ais_analysis',
+},{
+	title:'BMS状态',
+	dataIndex:'source_status',
+	key:'source_status',
+},{
+	title:'开始时间',
+	dataIndex:'start_time',
+	key:'start_time',
+},{
+	title:'结束时间',
+	dataIndex:'end_time',
+	key:'end_time',
+}
+]
 
 class TestApp extends Component {
 	constructor (props) {
 		super(props);
 		this.getData = this.getData.bind(this);
 		this.state = {url : null}
+		self=this;
 	}
 	getData () {
 		//var info;
@@ -27,11 +67,26 @@ class TestApp extends Component {
 			function(res){
 				//console.log('res : ',res.data);
 				info = res.data;
-				console.log("info : ",info);
+				if(info && info.length && info.length >0) {
+					info.map(
+						function(item,index){
+							item.key = index
+						}
+					)
+					self.setState({url:info});
+				}
+				
+				console.log("info : ",info instanceof Array );
 			}
 		);
 		
-		this.setState({url : info});
+		/*
+		info.foreach(function(item, index){
+			return item.key = index;
+		})
+		*/
+		console.log("info1 : ",info);
+		//this.setState({url : info});
 		//this.setState({url : data})
 		//get('/php/mysql.php?usr=徐晓静',function(res){console.log(res.data)})
 		/*
@@ -51,8 +106,9 @@ class TestApp extends Component {
 	render () {
 		return (
 			<div>
-				<p>{this.state.url}</p>
+				
 				<Button onClick={this.getData}>click me</Button>
+				<Table dataSource={info} columns={columns} />
 			</div>
 		);
 	}
